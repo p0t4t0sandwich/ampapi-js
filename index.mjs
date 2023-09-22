e734f967550b272ebd531875e08366975c8ca157
p0t4t0sandwich <p0t4t0sandwich@gmail.com>
2023-09-22 03:16:01 -0600

    Tweaked the lib layout

--- index.mjs
+++ index.mjs
@@ -1,21 +1,21 @@
 // Import AMPAPI
-import { AMPAPI } from "./lib/AMPAPI";
+import { AMPAPI } from "./lib/AMPAPI.js";
 
 // Import apimodules
-import { ADSModule } from "./lib/apimodules/ADSModule";
-import { Core } from "./lib/apimodules/Core";
-import { EmailSenderPlugin } from "./lib/apimodules/EmailSenderPlugin";
-import { FileManagerPlugin } from "./lib/apimodules/FileManagerPlugin";
-import { LocalFileBackupPlugin } from "./lib/apimodules/LocalFileBackupPlugin";
-import { MinecraftModule } from "./lib/apimodules/MinecraftModule";
-import { RCONPlugin } from "./lib/apimodules/RCONPlugin";
-import { steamcmdplugin } from "./lib/apimodules/steamcmdplugin";
+import { ADSModule } from "./lib/apimodules/ADSModule.js";
+import { Core } from "./lib/apimodules/Core.js";
+import { EmailSenderPlugin } from "./lib/apimodules/EmailSenderPlugin.js";
+import { FileManagerPlugin } from "./lib/apimodules/FileManagerPlugin.js";
+import { LocalFileBackupPlugin } from "./lib/apimodules/LocalFileBackupPlugin.js";
+import { MinecraftModule } from "./lib/apimodules/MinecraftModule.js";
+import { RCONPlugin } from "./lib/apimodules/RCONPlugin.js";
+import { steamcmdplugin } from "./lib/apimodules/steamcmdplugin.js";
 
 // Import modules
-import { ADS } from "./lib/modules/ADS";
-import { CommonAPI } from "./lib/modules/CommonAPI";
-import { GenericModule } from "./lib/modules/GenericModule";
-import { Minecraft } from "./lib/modules/Minecraft";
+import { ADS } from "./lib/modules/ADS.js";
+import { CommonAPI } from "./lib/modules/CommonAPI.js";
+import { GenericModule } from "./lib/modules/GenericModule.js";
+import { Minecraft } from "./lib/modules/Minecraft.js";
 
 // Import types
 import { ActionResult, AMPVersion, Branding, ConsoleEntry, CPUInfo, EndpointInfo, IADSInstance, Instance, InstanceDatastore, LoginResult, Message, Metric, ModuleInfo, PlatformInfo, RemoteTargetInfo, Result, RunningTask, SettingsSpec, Spec, State, lookupState, Status, Task, UpdateInfo, Updates, UserInfo, UUID } from "./lib/types.js";
--- lib/modules/Minecraft.ts
+++ lib/modules/Minecraft.ts
@@ -1,6 +1,6 @@
 /**
  * @author p0t4t0sandich
- * @description A TypeScript library for the AMP API
+ * @description An API that allows you to communicate with AMP installations from within JavaScript/TypeScript
  */
 
 import { MinecraftModule } from "../apimodules/MinecraftModule.js";
